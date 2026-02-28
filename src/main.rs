ac2c9e51fd8db9274086ac07f1cb7f5d7bf6ce21
Caufield, Greg <greg@thecaufields.ca>
2026-02-28 12:03:55 -0700

    Re-establish lost connections

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 //! Connects to our Bluetooth GATT service and exercises the characteristic.
 
-use bluer::{AdapterEvent, Device, Result, Uuid, gatt::remote::Characteristic};
+use bluer::{Adapter, AdapterEvent, Device, Result, Uuid, gatt::remote::Characteristic};
 use futures::{StreamExt, pin_mut};
 use std::time::Duration;
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    time::sleep,
-};
+use tokio::time::timeout;
 
 /// Service UUID for GATT example.
 const SERVICE_UUID: Uuid = Uuid::from_u128(0x0000181400001000800000805F9B34FB);
@@ -18,116 +15,117 @@ const CHARACTERISTIC_UUID: Uuid = Uuid::from_u128(0x00002A5300001000800000805F9B
 #[allow(dead_code)]
 const MANUFACTURER_ID: u16 = 0xf00d;
 
-async fn find_our_characteristic(device: &Device) -> Result<Option<Characteristic>> {
+async fn has_service(device: &Device) -> Result<bool> {
     let addr = device.address();
     let uuids = device.uuids().await?.unwrap_or_default();
     println!("Discovered device {} with service UUIDs {:?}", addr, &uuids);
     let md = device.manufacturer_data().await?;
     println!("    Manufacturer data: {:x?}", &md);
 
-    if uuids.contains(&SERVICE_UUID) {
-        println!("    Device provides our service!");
-
-        sleep(Duration::from_secs(2)).await;
-        if !device.is_connected().await? {
-            println!("    Connecting...");
-            let mut retries = 2;
-            loop {
-                match device.connect().await {
-                    Ok(()) => break,
-                    Err(err) if retries > 0 => {
-                        println!("    Connect error: {}", &err);
-                        retries -= 1;
-                    }
-                    Err(err) => return Err(err),
+    return Ok(uuids.contains(&SERVICE_UUID));
+}
+async fn connect_device(device: &Device) -> Result<()> {
+    if !device.is_connected().await? {
+        println!("    Connecting...");
+        let mut retries = 10;
+        loop {
+            match device.connect().await {
+                Ok(()) => break,
+                Err(err) if retries > 0 => {
+                    println!("    Connect error: {}", &err);
+                    retries -= 1;
                 }
-            }
-            println!("    Connected");
-        } else {
-            println!("    Already connected");
-        }
-
-        println!("    Enumerating services...");
-        for service in device.services().await? {
-            let uuid = service.uuid().await?;
-            println!("    Service UUID: {}", &uuid);
-            println!("    Service data: {:?}", service.all_properties().await?);
-            if uuid == SERVICE_UUID {
-                println!("    Found our service!");
-                for char in service.characteristics().await? {
-                    let uuid = char.uuid().await?;
-                    println!("    Characteristic UUID: {}", &uuid);
-                    println!(
-                        "    Characteristic data: {:?}",
-                        char.all_properties().await?
-                    );
-                    if uuid == CHARACTERISTIC_UUID {
-                        println!("    Found our characteristic!");
-                        return Ok(Some(char));
-                    }
-                }
+                Err(err) => return Err(err),
             }
         }
-
-        println!("    Not found!");
+        println!("    Connected");
+    } else {
+        println!("    Already connected");
     }
+    Ok(())
+}
+
+async fn find_rsc_characteristic(device: &Device) -> Result<Option<Characteristic>> {
+    println!("    Enumerating services...");
+    for service in device.services().await? {
+        let uuid = service.uuid().await?;
+        println!("    Service UUID: {}", &uuid);
+        println!("    Service data: {:?}", service.all_properties().await?);
+        if uuid == SERVICE_UUID {
+            println!("    Found our service!");
+            for char in service.characteristics().await? {
+                let uuid = char.uuid().await?;
+                println!("    Characteristic UUID: {}", &uuid);
+                println!(
+                    "    Characteristic data: {:?}",
+                    char.all_properties().await?
+                );
+                if uuid == CHARACTERISTIC_UUID {
+                    println!("    Found our characteristic!");
+                    return Ok(Some(char));
+                }
+            }
+        }
+    }
+
+    println!("    Not found!");
 
     Ok(None)
 }
 
 async fn exercise_characteristic(char: &Characteristic) -> Result<()> {
     println!("    Characteristic flags: {:?}", char.flags().await?);
-    sleep(Duration::from_secs(1)).await;
 
     if char.flags().await?.read {
         println!("    Reading characteristic value");
         let value = char.read().await?;
         println!("    Read value: {:x?}", &value);
-        sleep(Duration::from_secs(1)).await;
     }
 
     println!("    Starting notification session");
     {
         let notify = char.notify().await?;
         pin_mut!(notify);
-        for _ in 0..256u16 {
-            match notify.next().await {
-                Some(value) => {
-                    println!("    Notification value: {:x?}", &value);
-                }
-                None => {
+        loop {
+            match timeout(Duration::from_secs(3), notify.next()).await {
+                Ok(value) => match value {
+                    Some(val) => {
+                        println!("    Notification value: {:x?}", &val);
+                    }
+                    None => break,
+                },
+                Err(_) => {
                     println!("    Notification session was terminated");
+                    break;
                 }
             }
         }
         println!("    Stopping notification session");
     }
-    sleep(Duration::from_secs(1)).await;
-
-    println!("    Obtaining notification IO");
-    let mut notify_io = char.notify_io().await?;
-    println!("    Obtained notification IO with MTU={}", notify_io.mtu());
-    for _ in 0..5u8 {
-        let mut buf = vec![0; notify_io.mtu()];
-        match notify_io.read(&mut buf).await {
-            Ok(0) => {
-                println!("    Notification IO end of stream");
-                break;
-            }
-            Ok(read) => {
-                println!("    Notified with {} bytes: {:x?}", read, &buf[0..read]);
-            }
-            Err(err) => {
-                println!("    Notification IO failed: {}", &err);
-                break;
-            }
-        }
-    }
-    println!("    Stopping notification IO");
-    drop(notify_io);
-    sleep(Duration::from_secs(1)).await;
 
     Ok(())
+}
+
+async fn search_for_device(adapter: &Adapter) -> Result<Device> {
+    let discover = adapter.discover_devices().await?;
+    pin_mut!(discover);
+    while let Some(evt) = discover.next().await {
+        match evt {
+            AdapterEvent::DeviceAdded(addr) => {
+                let device = adapter.device(addr)?;
+                if has_service(&device).await? {
+                    println!("    Device provides our service!");
+                    return Ok(device);
+                }
+            }
+            _ => (),
+        }
+    }
+
+    Err(bluer::Error {
+        kind: bluer::ErrorKind::NotAvailable,
+        message: String::new(),
+    })
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -143,47 +141,32 @@ async fn main() -> bluer::Result<()> {
             adapter.name(),
             adapter.address().await?
         );
-        let discover = adapter.discover_devices().await?;
-        pin_mut!(discover);
-        let mut done = false;
-        while let Some(evt) = discover.next().await {
-            match evt {
-                AdapterEvent::DeviceAdded(addr) => {
-                    let device = adapter.device(addr)?;
-                    match find_our_characteristic(&device).await {
-                        Ok(Some(char)) => match exercise_characteristic(&char).await {
-                            Ok(()) => {
-                                println!("    Characteristic exercise completed");
-                                done = true;
-                            }
-                            Err(err) => {
-                                println!("    Characteristic exercise failed: {}", &err);
-                            }
-                        },
-                        Ok(None) => (),
-                        Err(err) => {
-                            println!("    Device failed: {}", &err);
-                            let _ = adapter.remove_device(device.address()).await;
-                        }
-                    }
-                    match device.disconnect().await {
-                        Ok(()) => println!("    Device disconnected"),
-                        Err(err) => println!("    Device disconnection failed: {}", &err),
-                    }
-                    println!();
-                }
-                AdapterEvent::DeviceRemoved(addr) => {
-                    println!("Device removed {addr}");
-                }
+
+        let device = search_for_device(&adapter).await?;
+
+        loop {
+            if device.is_connected().await? {
+                device.disconnect().await?;
+            }
+
+            match connect_device(&device).await {
+                Ok(()) => (),
+                Err(_) => continue,
+            }
+
+            let char = match find_rsc_characteristic(&device).await {
+                Ok(res) => match res {
+                    Some(char) => char,
+                    None => break,
+                },
+                Err(_) => continue,
+            };
+
+            match exercise_characteristic(&char).await {
                 _ => (),
             }
-            if done {
-                break;
-            }
         }
-        println!("Stopping discovery");
     }
 
-    sleep(Duration::from_secs(1)).await;
     Ok(())
 }
